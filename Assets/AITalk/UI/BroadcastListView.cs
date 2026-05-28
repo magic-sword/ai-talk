791588ccbc9fcb9f268691be2457af116dc45b54
magic-sword <magic.sword.0001@gmail.com>
2026-05-28 20:48:07 +0900

    UIをプレハブ化

--- Assets/AITalk/UI/BroadcastListView.cs
+++ Assets/AITalk/UI/BroadcastListView.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BroadcastListView : MonoBehaviour
 {
     [SerializeField]
-    private BroadcastView prefab;
+    private GameObject prefab;
 
     private List<BroadcastView> views = new();
+
+    /// <summary>
+    /// ライブ配信が選択された際に通知する
+    /// </summary>
+    public UnityEvent<YouTubeLiveController.LiveBroadcast> OnSelect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,9 +32,13 @@ public class BroadcastListView : MonoBehaviour
 
         foreach(var data in list)
         {
-            var view = Instantiate(prefab, this.transform);
-            view.Broadcast = data;
-            views.Add(view);
+            var view = Instantiate(prefab, this.transform); // プレハブからUIをクローン
+            var target = view.GetComponent<BroadcastView>();
+
+            target.Broadcast = data;    // UIへ表示データを設定
+            target.OnClick.AddListener(OnSelect.Invoke);    // クリック通知と選択通知を繋げる
+
+            views.Add(target);
         }
     }
 
